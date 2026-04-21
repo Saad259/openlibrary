973ecbb0f0f7191faf4c77c9e7dafc79c7cd8f30
Saad259 <saadlakdawala8@gmail.com>
2026-04-21 23:41:32 +0800

    Fixed history pagination indexing

--- openlibrary/plugins/upstream/recentchanges.py
+++ openlibrary/plugins/upstream/recentchanges.py
@@ -200,8 +200,8 @@ class history(delegate.mode):
         page = web.ctx.site.get(path)
         if not page:
             raise web.seeother(path)
-        i = web.input(page=0)
-        offset = 20 * safeint(i.page)
+        i = web.input(page=1)
+        offset = 20 * safeint((i.page) - 1)
         limit = 20
         history = get_changes({"key": path, "limit": limit, "offset": offset})
         return render.history(page, history)
